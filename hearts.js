3fd872ce876a799c1ef588ba2d633fee5b909402
Duong-Siu <duong9102001@gmail.com>
2025-06-02 11:30:27 +0700

    Add files via upload

---
+++ hearts.js
@@ -0,0 +1,15 @@
+const sky = document.getElementById('sky');
+
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.className = 'heart';
+  heart.style.left = Math.random() * 100 + 'vw';
+  heart.style.animationDuration = (2 + Math.random() * 3) + 's';
+  sky.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
+setInterval(createHeart, 200);
